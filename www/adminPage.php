fb1382226e22694380cb80f17cb40c4290da6736
Tori Roessler <tori.roessler@gmail.com>
2017-09-24 11:54:32 -0400

    9.24.17
    Contact page is now sending form contents to email address properly. Admin page is set up, but not displaying any records.

--- www/adminPage.php
+++ www/adminPage.php
@@ -1,8 +1,59 @@
 <?php
 //include the header
 include ('../includes/header.php');
+require_once '../includes/databaseConnect.php';
+
+$sql = "SELECT * from users";
+$query = $conn->query($sql);
+
+
+//Handle selection errors
+if (!$query) {
+    $errno = $conn->errno;
+    $errmsg = $conn->error;
+    echo "Selection failed with: ($errno) $errmsg<br/>\n";
+    $conn->close();
+    require_once ('../includes/footer.php');
+    exit;
+}
 
 ?>
+    <br>
+    <br>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+                <th>Email</th>
+                <th>Phone</th>
+                <th>Organization</th>
+                <th>Part #</th>
+                <th>Check Out Date</th>
+                <th>Status</th>
+            </tr>
+
+            <?php
+            //create a while loop here to insert one row for each user.
+            while (($row = $query->fetch_assoc()) !== NULL) {
+                echo "<tr>";
+                echo "<td>", $row['id'], "</td>";
+                echo "<td>", $row['firstname'], "</td>";
+                echo "<td>", $row['lastname'], "</td>";
+                echo "<td>", $row['username'], "</td>";
+                echo "<td>", $row['email'], "</td>";
+                echo "<td>", $row['phone'], "</td>";
+                echo "<td>", $row['organization'], "</td>";
+                echo "<td>", $row['partnumber'], "</td>";
+                echo "<td>", $row['checkoutdate'], "</td>";
+                echo "<td>", $row['status'], "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </table>
+
+
 
 
 
