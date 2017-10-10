5870262dafbb0f166d38ba42ab60a8c2051e870d
Tori Roessler <tori.roessler@gmail.com>
2017-10-10 18:13:42 -0400

    10/10 second commit
    -- admin page showing links based on users id (leads to user details page)

--- www/adminPage.php
+++ www/adminPage.php
@@ -41,7 +41,7 @@ if (!$query) {
             //create a while loop here to insert one row for each user.
             while (($row = $query->fetch_assoc()) !== NULL) {
                 echo "<tr>";
-                echo "<td>", $row['id'], "</td>";
+                echo "<td><a href='userDetails.php?id=" , $row['id'], "'>", $row['id'], "</a></td>";
                 echo "<td>", $row['firstname'], "</td>";
                 echo "<td>", $row['lastname'], "</td>";
                 echo "<td>", $row['username'], "</td>";
---
+++ www/userDetails.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tori
+ * Date: 10/10/17
+ * Time: 5:43 PM
+ */
