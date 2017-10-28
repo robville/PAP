ec74a04155dec9ae96d18aa713545ef767f1e2f5
robville <30539225+robville@users.noreply.github.com>
2017-10-28 08:47:06 -0400

    red ben
    Decided to try to get the statue pieces to color differently investigated that got it working. next step is to figure out an easy way to get object names by click event.

---
+++ includes/javaScript.php
@@ -0,0 +1,43 @@
+<?php
+
+//php data being passed to javascript
+$checkedOutArray = mysqli_query($conn, 'SELECT partnumber FROM users WHERE partnumber != ""'); // gets all checked out parts if any are in the users table
+
+
+$ToJS = array(); // an array to store all of the normalized parts from checkedOutArray
+
+if ($checkedOutArray->num_rows > 0){ //checks to make sure there is data before proceeding
+    while ($row = $checkedOutArray->fetch_assoc()){ //while loop exposes the associative array and assigns it to row
+        $ToJS[] = $row['partnumber']; //normalizes array from associative to a standard indexed array
+    }
+}
+?>
+
+
+<script type="text/javascript" language="JavaScript">
+
+    var checkedOut = <?php echo json_encode($ToJS);?>; //converts array from php to javascript
+
+
+    function checkedOutModelEffects() {//function called by the ready function supplied by the sketchfabAPIUtility
+        for (var i=0; i < checkedOut.length; i++) {
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.DiffuseColor,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.AOPBR,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.AlbedoPBR,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.BumpMap,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.CavityPBR,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.DiffusePBR,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.SpecularColor,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.SpecularF0,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.SpecularHardness,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+            sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.SpecularPBR,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
+
+
+            console.log(checkedOut[i]);
+        }
+    }
+
+
+
+
+</script>;
--- www/index.php
+++ www/index.php
@@ -2,42 +2,20 @@
 //include the header
 include ('../includes/header.php');
 include ('../includes/databaseConnect.php');
-
-
-
-$checkedOutArray = mysqli_query($conn, 'SELECT partnumber FROM users WHERE partnumber != ""');
-
-
-$ToJS = array();
-
-if ($checkedOutArray->num_rows > 0){
-    while ($row = $checkedOutArray->fetch_assoc()){
-        $ToJS[] = $row['partnumber'];
-    }
-}
-
+include ('../includes/javaScript.php');
 ?>
 
    <div class="contentWrapper">
     <iframe style="border:0" width="640" height="480" src="" id="api-frame" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true"></iframe>
    </div>
 
-
-<script type="text/javascript" language="JavaScript">
-
-    var checkedOut = <?php echo json_encode($ToJS);?>;
-
-    
-    function preLoadModelEffects() {
-        for (var i=0; i < checkedOut.length; i++) {
-            sketchfabAPIUtility.setAlpha(checkedOut[i], 0.5);
-            console.log(checkedOut[i]);
-        }
-    }
+  <script type="text/javascript" language="JavaScript">
 
         function  onSketchfabUtilityReady() {
 
-            preLoadModelEffects();
+<!--code inside this function gets run before the model loads-->
+
+            checkedOutModelEffects();
             }
 
         var sketchfabAPIUtility = new SketchfabAPIUtility('52070901286641fbbbd299b454f32c14', document.getElementById('api-frame'), onSketchfabUtilityReady);
