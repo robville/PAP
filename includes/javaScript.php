729a42ec35083bf02be631e8892d9b4ef0270569
robville <30539225+robville@users.noreply.github.com>
2017-11-22 19:10:28 -0500

    Revert "11/18"
    This reverts commit c9e9afe0aa0992390767cd98995bf35b0ae27ae4.

--- includes/javaScript.php
+++ includes/javaScript.php
@@ -19,7 +19,7 @@ if ($checkedOutArray->num_rows > 0){ //checks to make sure there is data before 
     var checkedOut = <?php echo json_encode($ToJS);?>; //converts array from php to javascript
 
 
-    function ModelFunctions() {             //function called by the ready function supplied by the sketchfabAPIUtility
+    function ModelFunctions() {//function called by the ready function supplied by the sketchfabAPIUtility
         for (var i=0; i < checkedOut.length; i++) {
             sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.DiffuseColor,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
             sketchfabAPIUtility.setColor(checkedOut[i], sketchfabAPIUtility.AOPBR,"#8a0000"); //assigns the material effect to each respective material as the for loop runs currently sets normal map color to dark red
@@ -39,42 +39,34 @@ if ($checkedOutArray->num_rows > 0){ //checks to make sure there is data before 
 
         }
 
+        sketchfabAPIUtility.api.addEventListener('click', function(event){
+
+            console.log(event);
+
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.DiffuseColor,"#ffff00");
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.AOPBR,"#ffff00");
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.AlbedoPBR,"#ffff00");
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.BumpMap,"#ffff00");
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.CavityPBR,"#ffff00");
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.DiffusePBR,"#ffff00");
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.SpecularColor,"#ffff00");
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.SpecularF0,"#ffff00");
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.SpecularHardness,"#ffff00");
+            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.SpecularPBR,"#ffff00");
+
+            if (event.material.name === "initialShadingGroup_2"){
+                console.log(event.material.name + "clicked");
+
+                function onSketchfabUtilityReady() {
 
 
+                }
+                var sceneChange = new SketchfabAPIUtility('b140631c005b41d593582c830e84e510', document.getElementById('api-frame'), onSketchfabUtilityReady);
 
 
-
-
-
-
-//        sketchfabAPIUtility.api.addEventListener('click', function(event){
-
-//            console.log(event);
-//
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.DiffuseColor,"#ffff00");
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.AOPBR,"#ffff00");
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.AlbedoPBR,"#ffff00");
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.BumpMap,"#ffff00");
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.CavityPBR,"#ffff00");
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.DiffusePBR,"#ffff00");
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.SpecularColor,"#ffff00");
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.SpecularF0,"#ffff00");
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.SpecularHardness,"#ffff00");
-//            sketchfabAPIUtility.setColor(event.material.name, sketchfabAPIUtility.SpecularPBR,"#ffff00");
-//
-//            if (event.material.name === "initialShadingGroup_2"){
-//                console.log(event.material.name + "clicked");
-//
-//                function onSketchfabUtilityReady() {
-//
-//
-//                }
-//                var sceneChange = new SketchfabAPIUtility('b140631c005b41d593582c830e84e510', document.getElementById('api-frame'), onSketchfabUtilityReady);
-//
-//
-//            }
-        }
-
+            }
+        });
+    }
 
 
 
